f24201e30bec47dc1c6128827bb28dec2d400c64
Wayne Chi <waynchi@gmail.com>
2014-08-02 15:18:25 +0800

    Header works and date and time added. Need to work on Buttons. #3. #4

--- inner/script.js
+++ inner/script.js
@@ -13,29 +13,46 @@ InnerNameSpace.myModule = function(){
 		Title: "Video Game Night",
 		Description: "Video Game Party featuring Super Smash Bros., League of Legends, Starcraft II, and more!",
 		Secondary: "Dinner will be served.",
-		Month: "July",
-		Day: "3",
-		Year: "2014",
+		Calendar: "7/16/2014",
 		Hour: "7" + " "+ "P.M.",
 		Time: "",
 		Location: "USC Mudd Hall Room 101"
 	};
 	
+	var test = "TEST";
+	
 	var init = function(){
-		event.Time = event.Hour + " on " + event.Month + ", " + event.Day + " , " + event.Year;
+		event.Time = event.Hour + " on " + event.Calendar;
 	};
 	
 	var EditClick = function(){
 		//This needs to open a page that allows you to edit event details.
 		window.open("", '_self', 'width=300, height=250');
 	}
-		
+	
+	var saveModal = function(){
+		console.log("Saving Modal");
+		event.Title = $('#eventTitle').value;
+		event.Description = $('#eventDescription').value;
+		event.Secondary = $('#eventSecondary').value;
+		event.Calendar = $('#datepicker').value;
+		event.Hour = $('#timepicker').value;
+		event.Time = event.Hour + " on " + event.Calendar;
+		event.Location = $('#eventLocation').value;
+	}
+	
+	var closeModal = function(){
+		console.log("Closing Modal");
+	}
 	
 	var oPublic = 
 	{
 		init: init,
 		event: event,
-		EditClick: EditClick
+		EditClick: EditClick,
+		closeModal: closeModal,
+		saveModal: saveModal,
+		test: test
 		/* eventTitle: eventTitle,
 		eventDescription: eventDescription,
 		eventTime: eventTime,
@@ -45,6 +62,27 @@ InnerNameSpace.myModule = function(){
 }();
 
 (function(){
-	$('#eventTitleForm').text = "Test";
+	$('#datepicker').datepicker();
+	
+	var t ;
+	$( document ).on(
+		'DOMMouseScroll mousewheel scroll',
+		'#myModal', 
+		function(){       
+			window.clearTimeout( t );
+			t = window.setTimeout( function(){            
+				$('#datepicker').datepicker('place')
+			}, 100 );        
+		}
+	);
+	$('#timepicker').timepicker({
+                minuteStep: 5,
+                showInputs: false,
+                disableFocus: true
+            });
 })();
 
+(function() {
+	$('#closeModalButton').click(function(){InnerNameSpace.myModule.closeModal()});
+	$('#saveModalButton').click(function(){InnerNameSpace.myModule.saveModal()});
+})();
