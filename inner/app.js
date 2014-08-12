3c045846bbe3b320dcda1b9b7ddf1a1017899c68
Wayne Chi <waynchi@gmail.com>
2014-08-12 16:26:24 +0800

    Changed Angular JS to now use scopes. #3

--- inner/app.js
+++ inner/app.js
@@ -1,12 +1,22 @@
 (function(){
 	var app = angular.module('inner',[]);
 	
-	app.controller('InnerController', function() {
+	app.controller('HeadingController', ['$scope', function($scope) {
 	
 		InnerNameSpace.myModule.init();
-		this.event = InnerNameSpace.myModule.event;
-		this.people = BodyNameSpace.myModule.people;
-		
-	});
-	BodyNameSpace.myModule.init();
+		$scope.event = InnerNameSpace.myModule.event;
+	}]);
+	
+	app.controller('BodyController', ['$scope', function($scope) {
+	
+		$scope.people = BodyNameSpace.myModule.people;
+	}]);
+	
+	app.directive('myRepeatDirective', function() {
+	  return function(scope, element, attrs) {
+		if (scope.$last){
+		  window.alert("im the last!");
+		}
+	  };
+	})
 })();
