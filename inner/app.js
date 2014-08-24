378fbbe99d2650ccbecfdfce7c61fa161bc12a13
Wayne <waynchi@gmail.com>
2014-08-24 14:36:20 -0700

    edit button for person entirely works. #8

--- inner/app.js
+++ inner/app.js
@@ -19,6 +19,7 @@
 	
 	app.controller('RepeatController', ['$scope', function($scope) {
 		$scope.active = false;
+		$scope.tempAttendance;
 		$scope.setActive = function(activater){
 			$scope.active = !$scope.active;		
 			var nameSelector = "[id='" + activater.name + 'Span' + "']"; 
@@ -41,6 +42,23 @@
 			//event.stopPropogation();
 		}
 		
+		//Saves changes for an edited person
+		$scope.saveChangePerson = function(person){
+			var tempModalID = '[id="edit' + person.name + 'Modal"]';
+			//New Name Variable
+			var tempNewNameID = '[id="edit' + person.name + 'ModalNewName"]';
+			var newName = $(tempNewNameID).val();
+			var tempAttendanceID = tempModalID + 'Attendance';
+			var tempAttendanceRadio =  'edit' + person.name + 'ModalAttendanceRadio';
+			var inputRadio = 'input:radio[name="' + tempAttendanceRadio + '"]';
+			console.log($scope.tempAttendance);
+			person.name = newName;
+			person.attending = $scope.tempAttendance;
+			
+			console.log(person.attending);
+			console.log("Saving Person in AngularJS");
+			
+		}
 	}]);
 	
 	app.directive('stopEvent', function () {
@@ -49,13 +67,18 @@
             link: function (scope, element, attr) {
                 element.bind(attr.stopEvent, function (e) {
                     e.stopPropagation();
-					var tempModalID = '#edit' + scope.person.name + 'Modal';
-					var tempNewNameID = tempModalID + 'NewName';
-					var tempAttendanceID = tempModalID + 'Attendance';
+					//variables to set edit modal to the default values
+					var tempModalID = '[id="edit' + scope.person.name + 'Modal"]';
+					var tempNewNameID = '[id="edit' + scope.person.name + 'ModalNewName"]';
+					var tempAttendanceID = '[id="edit' + scope.person.name + 'ModalAttendance"]';
 					var tempAttendanceRadio =  'edit' + scope.person.name + 'ModalAttendanceRadio';
-					var inputRadio = 'input:radio[name=' + tempAttendanceRadio + ']';
+					var inputRadio = 'input:radio[name="' + tempAttendanceRadio + '"]';
 					var inputRadioValue = '[value="' +  scope.person.attending + '"]';
-					
+					$(inputRadio).click(function() {
+						scope.tempAttendance = $(this).val();
+						console.log(scope.tempAttendance);
+					});
+					scope.tempAttendance = scope.person.attending;
 					$(inputRadio).filter(inputRadioValue).prop('checked', true);
 					$(tempModalID).modal('toggle');
 					$(tempNewNameID).val(scope.person.name);
